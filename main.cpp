5da03f41d60d29f311c3a169ee175bd36b99490b
Patrice André Waechter-Ebling <97856916+PatriceWaechterEbling@users.noreply.github.com>
2023-04-12 21:46:07 -0400

    Générateur UUID
    Générateur UUID pour VirtualBox 6 & 7

---
+++ main.cpp
@@ -0,0 +1,41 @@
+﻿#include "pch.h"
+
+#pragma warning(disable:4996)
+
+char* GenererUUID(size_t len)
+{
+    char tmp[0x40];
+    static const char x[] = "0123456789abcdef";
+    std::string uuid;
+    uuid.reserve(len);
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution < > dis(0, sizeof(x) - 2);
+    for (size_t i = 0; i < len; i++)
+        uuid += x[dis(gen)];
+    sprintf(tmp, "%s", uuid.c_str());
+    return tmp;
+}
+char* FormaterClefUUID(char* Sequence)
+{
+    char tmp[0x40];
+    std::string uuid;
+    int len = strlen(Sequence);
+    uuid.reserve(len);
+    for (size_t i = 0; i < len; i++) {
+        uuid += Sequence[i];
+        if (i == 8) { uuid += '-'; }
+        if (i == 13) { uuid += '-'; }
+        if (i == 18) { uuid += '-'; }
+        if (i == 23) { uuid += '-'; }
+    }
+    sprintf(tmp, "%s", uuid.c_str());
+    return tmp;
+}
+
+int main()
+{
+    printf("Generateur de UUID pour VirutualBox\t v:1.02!\t%c Patrice Waechter-Ebling 2022-2023\n",0xB8);
+    printf("\nUUID Machine      VirtualBox: %s", strupr(FormaterClefUUID(GenererUUID(32))));
+    printf("\nUUID Disque Dur   VirtualBox: %s", strupr(FormaterClefUUID(GenererUUID(32))));
+}
---
+++ pch.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#ifndef  WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif // ! WIN32_LEAN_AND_MEAN
+#ifndef WINVER
+#define _WIN32_WINNT 0x0A00
+#define WINVER 0x00A00
+#endif // !_WIN32_WINNT
+#define _Win32_DCOM
+
+#include <Windows.h>
+#include <CommCtrl.h>
+#include <tchar.h>
+#include <CommDlg.h>				// Dialogs
+#include <Shlwapi.h>				// e.g. For example for shortening long paths. Needed: Shlwapi.lib static linked.
+#include <strsafe.h>				// e.g. Safe string copy
+#include <ShellAPI.h>				// e.g. SHELLEXECUTEINFO
+#include <io.h>					// e.g. _access_s (file exists)
+#include <Shobjidl.h>				// COM objects
+#include <Objbase.h>
+#include <random>
